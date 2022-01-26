eda7bb873fac5cd51b3d8f2011ed2dd15390c160
Greg2111 <misterg410@gmail.com>
2022-01-26 12:09:15 +0000

    template implementation

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAZb35_83c0wIABKfb-_8XtZdZ0pVrrjcA",
+  authDomain: "house-rent-management-a6ed7.firebaseapp.com",
+  databaseURL: "https://house-rent-management-a6ed7-default-rtdb.firebaseio.com",
+  projectId: "house-rent-management-a6ed7",
+  storageBucket: "house-rent-management-a6ed7.appspot.com",
+  messagingSenderId: "908027993473",
+  appId: "1:908027993473:web:ef74f80c81d56f7cc41c80",
+  measurementId: "G-95VDJWYHP7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
