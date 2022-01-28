af5e6905aef4f8ca45d4690fe5510f95f8ff43f5
Gilles Yao <gillesyao12@gmail.com>
2022-01-28 15:09:41 +0000

    add firebase constant file

--- src/index.js
+++ src/index.js
@@ -4,20 +4,8 @@ import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 // import { getFirestore } from "firebase/firestore"
 import User from './models/user'
 import AuthService from './constants/auth_service'
+import { firebaseConfig } from './constants/firebase'
 
-console.log(getAuth)
-
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAZb35_83c0wIABKfb-_8XtZdZ0pVrrjcA",
-  authDomain: "house-rent-management-a6ed7.firebaseapp.com",
-  databaseURL: "https://house-rent-management-a6ed7-default-rtdb.firebaseio.com",
-  projectId: "house-rent-management-a6ed7",
-  storageBucket: "house-rent-management-a6ed7.appspot.com",
-  messagingSenderId: "908027993473",
-  appId: "1:908027993473:web:ef74f80c81d56f7cc41c80",
-  measurementId: "G-95VDJWYHP7"
-};
 const app = initializeApp(firebaseConfig);
 // const db = getFirestore();
 
